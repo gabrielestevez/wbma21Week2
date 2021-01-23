aa01894bacea704078b1deca178523374dec25eb
Gabriel Estevez Cabello <g.esteves@usp.ceu.es>
2021-01-23 15:25:19 +0200

    navigation

---
+++ components/ListItem.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+import {uploadsUrl} from '../utils/Variables';
+
+const ListItem = ({navigation, singleMedia}) => {
+  // console.log(props);
+  return (
+    <TouchableOpacity style={styles.row}>
+      onPress=
+      {() => {
+        navigation.navigate('Single');
+      }}
+      <View style={styles.imagebox}>
+        <Image
+          style={styles.image}
+          source={{uri: uploadsUrl + singleMedia.thumbnails.w160}}
+        />
+      </View>
+      <View style={styles.textbox}>
+        <Text style={styles.listTile}>{singleMedia.title}</Text>
+        <Text>{singleMedia.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: '#eee',
+    borderRadius: 6,
+  },
+  imagebox: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    borderRadius: 6,
+  },
+  textbox: {
+    flex: 2,
+    padding: 10,
+  },
+  listTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 15,
+  },
+});
+
+ListItem.propTypes = {
+  singleMedia: PropTypes.object,
+  navigation: PropTypes.object,
+};
+
+export default ListItem;
