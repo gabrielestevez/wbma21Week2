24f3f4975a312ab4f7ab26c5f818aed36dd09f1a
Gabriel Estevez Cabello <g.esteves@usp.ceu.es>
2021-01-24 22:26:41 +0200

    task 6

---
+++ components/LoginForm.js
@@ -0,0 +1,47 @@
+import React, {useContext, useEffect} from 'react';
+import {View, Button} from 'react-native';
+import PropTypes from 'prop-types';
+import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useLogin} from '../hooks/ApiHooks';
+import FormTextInput from './FormTextInput';
+import useLoginForm from '../hooks/LoginHooks';
+
+const LoginForm = ({navigation}) => {
+  const {inputs, handleInputChange} = useLoginForm();
+  const {postLogin} = useLogin();
+  const [isLoggedIn, setIsLoggedIn] = useContext(MainContext);
+  const doLogin = async () => {
+    try {
+      const userData = await postLogin(inputs);
+      setIsLoggedIn(true);
+      await AsyncStorage.setItem('userToken', userData.token);
+
+    } catch (error) {
+      console.error('postLogin error', error);
+      // TODO
+    }
+  return (
+    <View>
+      <FormTextInput
+        autoCapitalize="none"
+        placeholder="username"
+        onChangeText={(txt) => handleInputChange('username', txt)}
+      />
+      <FormTextInput
+        autoCapitalize="none"
+        placeholder="password"
+        onChangeText={(txt) => handleInputChange('password', txt)}
+        secureTextEntry={true}
+      />
+      <Button title="Login" onPress={doLogin} />
+    </View>
+  );
+};
+
+
+LoginForm.PropTypes = {
+  navigation: PropTypes.object,
+};
+
+export default LoginForm;
---
+++ components/RegisterForm.js
@@ -0,0 +1,63 @@
+import React, {useContext, useEffect} from 'react';
+import {Alert, Button, View} from 'react-native';
+import PropTypes from 'prop-types';
+import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useLogin, useRegister} from '../hooks/ApiHooks';
+import FormTextInput from './FormTextInput';
+import useSignUpForm from '../hooks/RegisterHooks';
+
+const RegisterForm = ({navigation}) => {
+  const {setIsLoggedIn, setUser} = useContext(MainContext);
+  const {inputs, handleInputChange} = useSignUpForm();
+  const {postRegister} = useRegister();
+  const {postLogin} = useLogin();
+  const doRegister = async () => {
+    try {
+      const result = await postRegister(inputs);
+      console.log('doRegister ok', result.message);
+      Alert.alert(result.message);
+      postLogin(inputs);
+      // TODO: login automatically
+      const userData = await postLogin(inputs);
+      await AsyncStorage.setItem('userToken', userData.token);
+      setIsLoggedIn(true);
+      setUser(userData.user);
+    } catch (error) {
+      console.log('registration error', error);
+      Alert.alert(error.message);
+    }
+  };
+  return (
+    <View>
+      <FormTextInput
+        autoCapitalize="none"
+        placeholder="username"
+        onChangeText={(txt) => handleInputChange('username', txt)}
+      />
+      <FormTextInput
+        autoCapitalize="none"
+        placeholder="password"
+        onChangeText={(txt) => handleInputChange('password', txt)}
+        secureTextEntry={true}
+      />
+      <FormTextInput
+        autoCapitalize="none"
+        placeholder="email"
+        onChangeText={(txt) => handleInputChange('email', txt)}
+      />
+      <FormTextInput
+        autoCapitalize="none"
+        placeholder="full name"
+        onChangeText={(txt) => handleInputChange('full_name', txt)}
+      />
+      <Button title="Register!" onPress={doRegister} />
+    </View>
+  );
+};
+
+RegisterForm.PropTypes = {
+  navigation: PropTypes.object,
+};
+
+export default RegisterForm;
